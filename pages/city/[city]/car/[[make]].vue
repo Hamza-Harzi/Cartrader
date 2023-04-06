89a8eded9af627ab621f9d5e33e92111b972ca90
Hamza-Harzi <97437135+Hamza-Harzi@users.noreply.github.com>
2023-04-06 20:46:04 +0200

    working with useFetchCars

--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -1,15 +1,22 @@
 <script setup>
 const route = useRoute();
-const cars = await useFetchCars(route.params.city, {
+const { data: cars, refresh } = await useFetchCars(route.params.city, {
   minPrice: route.query.minPrice,
   maxPrice: route.query.maxPrice,
   make: route.params.make,
 });
-console.log(cars);
+
+watch(
+  () => route.query,
+  () => {
+    window.location.reload(true);
+  }
+);
 </script>
 
 <template>
   <div>
-    <CarCards :cars="cars" />
+    <CarCards v-if="cars.length" :cars="cars" />
+    <h1 v-else class="text-red-600">No Cars Found With Filters</h1>
   </div>
 </template>
