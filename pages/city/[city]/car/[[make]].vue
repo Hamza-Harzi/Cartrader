19856d7e308faf77ea6d3a719dc2c9b86dbd1777
Hamza-Harzi <97437135+Hamza-Harzi@users.noreply.github.com>
2023-04-06 02:18:55 +0200

    using the useFetch and passing the propes to the componnent

--- pages/city/[city]/car/[[make]].vue
+++ pages/city/[city]/car/[[make]].vue
@@ -1,8 +1,15 @@
 <script setup>
+const route = useRoute();
+const cars = await useFetchCars(route.params.city, {
+  minPrice: route.query.minPrice,
+  maxPrice: route.query.maxPrice,
+  make: route.params.make,
+});
+console.log(cars);
 </script>
 
 <template>
   <div>
-    <CarCards />
+    <CarCards :cars="cars" />
   </div>
 </template>
