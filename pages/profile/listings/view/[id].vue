4099654dea19428da10facc617b0176a5c106b94
Hamza-Harzi <97437135+Hamza-Harzi@users.noreply.github.com>
2023-04-08 08:10:18 +0200

    A GET Message Endpoint

--- pages/profile/listings/view/[id].vue
+++ pages/profile/listings/view/[id].vue
@@ -4,13 +4,20 @@ definePageMeta({
   // nesta3mlo l middleware bich nehmiw l route wela les page mte3na maynajem yode5lelhom ken ma ya3mel login
   // middleware: 3ayetna lil auth min lfolder middleware li fi westo l file auth
 });
+
+const route = useRoute();
+const { data: messages } = useFetch(
+  `/api/car/listings/${route.params.id}/message`
+);
 </script>
 
 <template>
   <div class="rounded shadow mt-20">
-    <CarMessageCard />
-    <CarMessageCard />
-    <CarMessageCard />
-    <CarMessageCard />
+    {{ data }}
+    <CarMessageCard
+      v-for="message in messages"
+      :key="message.id"
+      :message="message"
+    />
   </div>
 </template>
