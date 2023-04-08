ce84faf87770281b7714b152084f69db8b4a82f2
Hamza-Harzi <97437135+Hamza-Harzi@users.noreply.github.com>
2023-04-08 15:43:58 +0200

    Storing Images in a Bucket

--- pages/profile/listings/create.vue
+++ pages/profile/listings/create.vue
@@ -6,6 +6,7 @@ definePageMeta({
 
 const { makes } = useCars();
 const user = useSupabaseUser();
+const supabase = useSupabaseClient("");
 
 const info = useState("adInfo", () => {
   return {
@@ -18,7 +19,7 @@ const info = useState("adInfo", () => {
     seats: "",
     features: "",
     description: "",
-    image: "afafafaf",
+    image: null,
   };
 });
 
@@ -81,6 +82,14 @@ const isButtonDisabled = computed(() => {
 });
 
 const handleSubmit = async () => {
+  const fileName = Math.floor(Math.random() * 100000000000000000000);
+  const { data, error } = await supabase.storage
+    .from("images")
+    .upload("public/" + fileName, info.value.image);
+
+  if (error) {
+    return (errorMessage.value = "cannot uplod images");
+  }
   const body = {
     ...info.value,
     city: info.value.city.toLowerCase(),
@@ -91,7 +100,7 @@ const handleSubmit = async () => {
     year: parseInt(info.value.year),
     name: `${info.value.make} ${info.value.model}`,
     listerId: user.value.id,
-    image: "sscshcjbubveub",
+    image: data.path,
   };
 
   delete body.seats;
@@ -104,6 +113,7 @@ const handleSubmit = async () => {
     navigateTo("/profile/listings");
   } catch (err) {
     errorMessage.value = err.statusMessage;
+    await supabase.storage.from("images").remove(data.path);
   }
 };
 </script>
