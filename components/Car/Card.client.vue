e01dcf1799e57441f6ab0709930a691e1fba715a
Hamza-Harzi <97437135+Hamza-Harzi@users.noreply.github.com>
2023-04-08 07:58:44 +0200

    Fixing the GET Endpoints

--- components/Car/Card.client.vue
+++ components/Car/Card.client.vue
@@ -11,16 +11,7 @@ const emit = defineEmits(["favor"]);
 
 <template>
   <div
-    class="
-      relative
-      shadow
-      border
-      w-full
-      overflow-hidden
-      mb-5
-      cursor-pointer
-      h-[200px]
-    "
+    class="relative shadow border w-full overflow-hidden mb-5 cursor-pointer h-[200px]"
   >
     <img
       class="absolute w-7 right-5 top-2 z-20"
@@ -29,7 +20,7 @@ const emit = defineEmits(["favor"]);
       @click="emit('favor', car.id)"
     />
     <div class="flex h-full" @click="navigateTo(`/car/${car.name}-${car.id}`)">
-      <NuxtImg :src="car.url" class="w-[300px] h-full" />
+      <NuxtImg :src="car.image" class="w-[300px] h-full" />
       <div class="p-4 flex flex-col">
         <div>
           <h1 class="text-2xl text-blue-700">{{ car.name }}</h1>
